d2a695a06ca42c7f37f6963059df0ce70c2b099b
Topper <serhii.hruntkovskii@gmail.com>
2023-08-14 13:37:42 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+
+    let stack = [];
+
+    for (i = 0; i < str.length; i++) {
+        const match = bracketsConfig.find(bracketsConfig => bracketsConfig.includes(str[i]));
+
+        if (str[i] == match[1] && match[0] == stack[stack.length - 1]) {
+            stack.pop();
+        }
+        else
+        {
+            stack.push(str[i]);
+        }
+    }
+
+    return !stack.length;
 }
